d8adc4ab46e2b6e558a63daff5fe312b6bb55763
Artem <artemmocrousov1@gmail.com>
2026-03-22 16:26:59 +0500

    add db

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"log"
+	"os"
+
+	"github.com/Wartemelon/TODO-list/pkg/db"
 	"github.com/Wartemelon/TODO-list/pkg/server"
 )
 
 func main() {
+	dbFile := os.Getenv("TODO_DBFILE")
+	if dbFile == "" {
+		log.Fatalln("Environmental value: TODO_DBFILE must be not empty")
+	}
+
+	err := db.Init(dbFile)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	server.Run()
 }
